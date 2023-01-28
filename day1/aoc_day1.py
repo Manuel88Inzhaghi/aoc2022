b78c0109cceac35f8732ac43cf328a640553f0f7
Manuel88Inzhaghi <121864609+Manuel88Inzhaghi@users.noreply.github.com>
2023-01-28 16:20:05 +0000

    Add files via upload

---
+++ day1/aoc_day1.py
@@ -0,0 +1,26 @@
+with open("day_1t.txt") as f:
+
+  list_of_cals = []
+  cals_per_elf = 0
+
+  for cal in f:
+   
+    if len(cal) > 1:  
+      food = int(cal)  
+      cals_per_elf += food
+    else:
+      list_of_cals.append(cals_per_elf)  
+      cals_per_elf = 0 
+      
+  sorted_cals = sorted(list_of_cals)  
+  sorted_cals.reverse()
+
+  # Part 1
+  print(sorted_cals[0]) 
+
+  # Part 2
+  print(sum(sorted_cals[0:3]))
+
+  # Just to know the number of elves
+  print(f'There were {len(list_of_cals)} elves in all.')
+
